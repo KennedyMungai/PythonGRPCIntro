fca4cdd00e0b1c55d8edccbe86436cb86985bc91
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-04 11:24:25 +0300

    Using a and b instead of x and y

--- client.py
+++ client.py
@@ -4,10 +4,10 @@ import calculator_pb2
 import calculator_pb2_grpc
 
 def run():
-    channel = grpc.insecure_server_credentials("localhost:50051")
+    channel = grpc.insecure_channel('localhost:50051')
     stub = calculator_pb2_grpc.CalculatorStub(channel)
     
-    request = calculator_pb2.AddRequest(x=10, y=20)
+    request = calculator_pb2.AddRequest(a=10, b=20)
     response = stub.Add(request)
     
     print("Result: ", response.result)
